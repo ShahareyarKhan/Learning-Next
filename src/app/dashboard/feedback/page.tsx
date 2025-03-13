7f4601236745b4d2663fd78b8deec8fc10e30a37
ShahareyarKhan <shahareyar2003@gmail.com>
2025-03-14 05:08:35 +0530

    commit

---
+++ src/app/dashboard/feedback/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 w-full font-[family-name:var(--font-geist-sans)]">
+      Feedback Dashboard
+    </div>
+  );
+}
